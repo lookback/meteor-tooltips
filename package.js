0366123b77edb2f74e481340eb58680e89f39b6c
Johan Brook <johan.jvb@gmail.com>
2015-01-28 17:14:58 +0100

    v0.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'lookback:tooltips',
   summary: 'Reactive tooltips.',
-  version: '0.1.2',
+  version: '0.2.0',
   git: 'https://github.com/lookback/meteor-tooltips.git'
 })
 
@@ -10,4 +10,5 @@ Package.on_use(function(api) {
   api.use('coffeescript jquery templating tracker'.split(' '), 'client')
 
   api.add_files('tooltips.html tooltips.coffee'.split(' '), 'client')
+  api.export('Tooltips', 'client')
 })
